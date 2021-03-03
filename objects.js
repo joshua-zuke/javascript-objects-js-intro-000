d82e55b8a99c36e2f75707b1845eee22ba6aa418
Joshua Zuke <zuke.joshua@gmail.com>
2021-03-03 20:04:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,7 +8,7 @@ function updatePlaylist(obj, key, value){
   return obj
 }
 
-function removeFromPlaylist(playlist, key){
-  delete playlist.key
-  return playlist
+function removeFromPlaylist(obj, key){
+  delete obj.key
+  return obj
 }
